07a8387469f6ee322d05e7bd6289e05ddece88c2
Tito Reinhart <TKral@pdx.edu>
2023-02-16 18:49:30 -0800

    updating ...

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -62,7 +62,7 @@ export default defineConfig({
 			// Optional, default: 'viteNodeApp'
 			// the name of named export of you app from the appPath file
 			// this has to match the last line in src/server.ts where we export our final app
-			exportName: "doggr",
+			exportName: "petorpass",
 
 			// Optional, default: 'esbuild', using swc for TypeORM support
 			tsCompiler: "swc",
