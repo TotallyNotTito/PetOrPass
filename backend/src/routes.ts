2ce09a8949874092a6489d241f4f0ea0dc27f51e
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-02-19 20:56:22 -0800

    implemented root route with temporary placeholder static html file to be replaced with serving react app

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,7 +2,7 @@
 
 import cors from "cors";
 import {FastifyInstance, FastifyReply, FastifyRequest} from "fastify";
-// import {Pet} from "./db/models/pet";
+import {Pet} from "./db/models/pet";
 
 /**
  * App plugin where we construct our routes
@@ -14,11 +14,26 @@ export async function pet_routes(app: FastifyInstance): Promise<void> {
 	app.use(cors());
 
 	/**
+	 * Root route to serve landing page of app
+	 * @name get/root
+	 * @function
+	 */
+	app.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
+		reply.code(200)
+		// TODO: This is a placeholder static html file until we implement and serve react front end
+		await reply.sendFile("index.html");
+	});
+
+
+
+
+	/**
 	 * Route replying to /test path for test-testing
 	 * @name get/test
 	 * @function
 	 */
 	app.get("/test", async (request: FastifyRequest, reply: FastifyReply) => {
-		reply.send("GET Test");
+		reply.code(200)
+		await reply.send("GET Test");
 	});
 }
