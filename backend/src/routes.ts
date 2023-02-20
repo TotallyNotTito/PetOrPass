e7f2ce43a403b0e362e77766f869e7ff316ab5c2
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-02-20 00:06:28 -0800

    implement POST route for creating new pet in database

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@
 import cors from "cors";
 import {FastifyInstance, FastifyReply, FastifyRequest} from "fastify";
 import {Pet} from "./db/models/pet";
+import {faker} from "@faker-js/faker";
 
 /**
  * App plugin where we construct our routes
@@ -45,6 +46,35 @@ export async function pet_routes(app: FastifyInstance): Promise<void> {
 		reply.code(200)
 		await reply.send("PLACEHOLDER for Logout via Authentication Microservice");
 	});
+
+	/**
+	 * Route to create new pet details in database and store pet image in file storage
+	 * @name post/pet
+	 * @function
+	 * @param {string} pet_name - name of submitted pet
+	 * @param {image file} image_file - image of submitted pet
+	 * @param {string} submitted_by - auth ID of user that submitted pet
+	 */
+	app.post("/pet", async (request: FastifyRequest, reply: FastifyReply) => {
+		const {pet_name, submitted_by} = req.body;
+		const image_data = await request.file();
+		const image_name = `${faker.animal.type()}${faker.datatype.uuid()}.${data.mimetype}`;
+
+		const pet = new Pet();
+		pet.pet_name = pet_name;
+		pet.image_name = image_name;
+		pet.total_score = 0;
+		pet.total_votes = 0;
+		pet.submitted_by = submitted_by;
+		await pet.save();
+
+		// TODO: Pet image will be saved to MinIO file storage with new name once file storage implemented
+
+		reply.code(201)
+		await reply.send();
+	});
+
+
 
 
 	/**
