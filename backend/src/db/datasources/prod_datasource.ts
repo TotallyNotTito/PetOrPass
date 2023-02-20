cd1146578f92f78e46119efffa9409532c875cb6
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-02-19 19:29:20 -0800

    update prod version of datasource with initialization migration

--- backend/src/db/datasources/prod_datasource.ts
+++ backend/src/db/datasources/prod_datasource.ts
@@ -1,7 +1,7 @@
 // import dotenv from "dotenv";
 // import {DataSource} from 'typeorm';
 // import {Pet} from "../models/pet";
-// import {Initialize1676281754950} from "../migrations/1676281754950-Initialize.ts";
+// import {Initialize1676862032034} from "../migrations/1676862032034-Initialize.ts";
 //
 // dotenv.config();
 //
@@ -21,7 +21,7 @@
 // 			Pet,
 // 		],
 // 		migrations: [
-// 			Initialize1676281754950
+// 			Initialize1676862032034
 // 		],
 // 		// DANGER DANGER our convenience will nuke production data!
 // 		synchronize: false,
