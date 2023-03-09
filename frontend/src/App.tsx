010d6873cfa9bb867d27f8dbd98a10110f92fb08
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-03-09 09:50:15 -0800

    set up top level files in src directory, including initial configuration of FE router

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import {PetOrPassRoutes} from "./Router";
+
+function App() {
+    return (
+        <PetOrPassRoutes/>
+    )
+}
+
+export default App
---
+++ frontend/src/initialState.ts
@@ -0,0 +1,21 @@
+// TODO: Figure out if this file is necessary if we don't have any of our own initial state
+
+// import {State, ProfileType} from "./types/DoggrTypes";
+//
+// const initialState: { likeHistory: ProfileType[]; currentProfile: ProfileType } = {
+//     currentProfile: getRandomProfile(),
+//     likeHistory: [getRandomProfile(), getRandomProfile()],
+// };
+//
+// export default initialState;
+//
+// export function getRandomProfile(): ProfileType {
+//     const idNum = Math.random() * 10000;
+//
+//     return {
+//         imgUri: `https://loremflickr.com/300/300/animal?lock=${idNum}`,
+//         thumbUri: `https://loremflickr.com/75/75/animal?lock=${idNum}`,
+//         name: `Doggr${idNum}`,
+//         id: idNum,
+//     };
+// }
