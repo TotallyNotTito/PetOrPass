38aa948d8ac464be98098dfee4b3745a35194aee
Tito Reinhart <TKral@pdx.edu>
2023-03-11 13:22:49 -0800

    Revert "submit pet"
    This reverts commit 55ded7386dadd9b942beb90fdce7b81e9bc1def5.

--- frontend/src/components/SubmitPetForm.tsx
+++ frontend/src/components/SubmitPetForm.tsx
@@ -1,29 +1,3 @@
 export function SubmitPetForm() {
-    return (
-        <form className="container below-navbar">
-            <div className="row text-center">
-                <h1>Submit Your Pet!</h1>
-            </div>
-            <div className="row text-center">
-                <legend>Submit an image of your pet to be rated by other users</legend>
-            </div>
-            <div className="row mt-3 justify-content-center">
-                <div className="col-xl-4 col-md-6 col-sm-8 col-10">
-                    <label className="form-label" htmlFor="petNameInput">Pet Name</label>
-                    <input className="form-control" type="text" id="petNameInput" name="petNameInput"/>
-                </div>
-            </div>
-            <div className="row mt-3 justify-content-center">
-                <div className="col-xl-4 col-md-6 col-sm-8 col-10">
-                    <label className="form-label" htmlFor="petImageInput">Upload Pet Image</label>
-                    <input className="form-control" type="file" id="petImageInput" name="petImageInput" accept="image/*"/>
-                </div>
-            </div>
-            <div className="row mt-5 justify-content-center">
-                <div className="col-xl-4 col-md-6 col-sm-8 col-10">
-                    <button className="btn btn-lg button-color w-100" type="submit">Submit!</button>
-                </div>
-            </div>
-        </form>
-    );
+    return (<div className="below-navbar">SUBMIT PET FORM PAGE PLACEHOLDER</div>);
 }
