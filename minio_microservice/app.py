078e9615427f130cdc7fd99e08f50b0d680d8bff
Tito Reinhart <TKral@pdx.edu>
2023-03-11 18:13:29 -0800

    working on routing

--- minio_microservice/app.py
+++ minio_microservice/app.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from backend.minio import file_storage
+from backend.minio.file_storage import FileDumper
 from flask import Flask
 from flask import request, redirect
 import os
@@ -12,6 +12,11 @@ app = Flask(__name__)
 def load_images():
     test_dict = {'dog':'poodle'}
     return test_dict
+
+@app.route("/SubmitPetForm")
+def add_image():
+    client = FileDumper('tito','cute.jpg','file/path/cute.jpg','pet-images')
+    client.add_image()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
