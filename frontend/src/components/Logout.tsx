99397f3eac92f10144c8f941131372e561106df2
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-03-13 14:43:48 -0700

    finalize log out component

--- frontend/src/components/Logout.tsx
+++ frontend/src/components/Logout.tsx
@@ -1,12 +1,13 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from "@auth0/auth0-react";
 import React from "react";
+import {useEffect} from "react";
 
 export function Logout() {
     const { logout } = useAuth0();
 
-    return (
-        <button className="below-navbar" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-            Log Out
-        </button>
-    );
+    useEffect(() => {
+        logout({ logoutParams: { returnTo: window.location.origin } });
+    });
+
+    return (<></>);
 };
