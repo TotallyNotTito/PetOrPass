dbc53629da1f7b30e9b7e10f613993d50dabeb2e
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-03-13 13:49:26 -0700

    add text reminding users to use real email address to enable future pw resets

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -17,8 +17,11 @@ export function Login() {
                     <div className="row align-items-center justify-content-center mt-4">
                         <h1 className="display-4 lead fw-normal text-center">Pet or Pass</h1>
                     </div>
-                    <div className="row align-items-center justify-content-center my-4">
+                    <div className="row align-items-center justify-content-center mt-4">
                         <p className="text-center fw-bold">Create an account and login to view and rate pets! Will you pet them or will you take a pass?</p>
+                    </div>
+                    <div className="row align-items-center justify-content-center mb-2">
+                        <p className="text-center">Reminder: You must sign up with a functional email address to allow password reset if you forget your password</p>
                     </div>
                     <div className="row justify-content-center align-items-center my-4">
                         <button className="btn btn-lg button-color w-100" onClick={() => loginWithRedirect()}>Enter Site!</button>
