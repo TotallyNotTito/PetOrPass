59268148471ffb11938c3146545b34223ae5662e
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-03-15 19:18:49 -0700

    refactor static html/css of error message page into react

--- frontend/src/components/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage.tsx
@@ -1,3 +1,6 @@
+import errorImage from "../assets/error_message.svg";
+import React from "react";
+
 export type ErrorProps = {
     errorMessage: string,
 }
@@ -5,8 +8,20 @@ export function ErrorMessage(props: ErrorProps) {
     let {errorMessage} = props;
 
     return (
-        <div className="below-navbar">
-            {errorMessage}
-        </div>
+        <main class="container below-navbar text-center">
+            <div class="row">
+                <img
+                    src={errorImage}
+                    alt="404 error message"
+                    title="404 error message"
+                    class="mx-auto img-fluid col-10 col-sm-8 col-md-7 col-lg-5"
+                />
+            </div>
+            <div class="row justify-content-center">
+                <h1 class="display-6 col-12 col-lg-10 mt-5">
+                    {errorMessage}
+                </h1>
+            </div>
+        </main>
     );
 }
--- frontend/src/components/RatePet.tsx
+++ frontend/src/components/RatePet.tsx
@@ -1,3 +1,5 @@
 export function RatePet() {
     return (<div className="below-navbar">RATE PET PAGE PLACEHOLDER</div>);
 }
+
+// uh-oh! Looks like no one has submitted any pets yet for rating! You can be the first by visiting the Submit Pet tab.
