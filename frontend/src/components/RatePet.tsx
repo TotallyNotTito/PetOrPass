f578090ca453c6b3ba7dcc71362d3c6c7d9808d2
Kelsey Werner <kelsey.lee.werner@gmail.com>
2023-03-19 16:54:19 -0700

    a little typescript clean up

--- frontend/src/components/RatePet.tsx
+++ frontend/src/components/RatePet.tsx
@@ -30,8 +30,8 @@ export function RatePet() {
 
         // Verify that correct key for accessing jwt exists in local storage
         // and if not, log out and redirect to login page
-        let foundKey = false;
-        storageKeys.forEach((key)=>{
+        let foundKey: string | boolean = false;
+        storageKeys.forEach((key: string)=>{
             if (key.includes("@@user@@")) {
                 foundKey = key;
             }
